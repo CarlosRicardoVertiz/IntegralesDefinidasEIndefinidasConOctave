1cb28c19809b001a5f2855e1aa8b3e56062fefd8
@RicardoVert <82452690+CarlosRicardoVertiz@users.noreply.github.com>
2021-05-21 23:22:17 -0500

    Rename IntegralDefinida3CarlosRicardoVertiz3202.m to FuncionPrimitiva3CarlosRicardoVertiz.m

--- FuncionPrimitiva3CarlosRicardoVertiz.m
+++ FuncionPrimitiva3CarlosRicardoVertiz.m
@@ -11,8 +11,8 @@ clc
 syms x;
 % f tendra el valor que se digite
 f =abs(x+2);
-%inline: Las funciones en línea 
-%se pueden definir en cualquier parte de un código
+%inline: Las funciones en lÃ­nea 
+%se pueden definir en cualquier parte de un cÃ³digo
 F= inline(char(f));
 %intervalos en los que se evalua la integral
 a=-4;
